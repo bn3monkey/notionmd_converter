a7f2e6f8d7acf4b354d0fc38851a347ab5b4d3e6
bn3monkey <bn3monkey@naver.com>
2024-02-06 17:56:52 +0900

    fix batch file

--- main.bat
+++ main.bat
@@ -1,4 +1,10 @@
-source venv/Scripts/activate
+python -m venv venv
+echo "Create Virtual Environment"
+call ./venv/Scripts/activate.bat
+echo "Virtual Environment Activated"
 pip install -r requirements.txt
+echo "Dependency installed"
 python main.py
+echo "Run"
 deactivate
+echo "Deactivate"
